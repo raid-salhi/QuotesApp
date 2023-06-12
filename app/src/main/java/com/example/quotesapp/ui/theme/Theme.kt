42d8c1157a4e79d284578d2bda79d56bc61c1558
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-06-12 13:44:48 +0100

    added some theme

--- app/src/main/java/com/example/quotesapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/quotesapp/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -13,24 +14,24 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xff8c7851),
+    primaryVariant = Color(0xff716040),
+    secondary = Color(0xffeaddcf),
 
-    /* Other default colors to override
-    background = Color.White,
+
+    background = Color(0xfff9f4ef),
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+
 )
 
 @Composable
 fun QuotesAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
