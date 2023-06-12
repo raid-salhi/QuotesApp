8c837e2a06111da0a880f9245ff05db4939452e8
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-06-12 17:10:55 +0100

    finished with main screen

--- app/src/main/java/com/example/quotesapp/screens/mainSceen/MainScreen.kt
+++ app/src/main/java/com/example/quotesapp/screens/mainSceen/MainScreen.kt
@@ -1,55 +1,116 @@
 package com.example.quotesapp.screens.mainSceen
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AddCircle
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFontFamilyResolver
-import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.quotesapp.R
+import com.example.quotesapp.model.Quote
 import com.example.quotesapp.model.QuoteItem
 
 @Composable
 fun MainScreen(mainScreenViewmodel: MainScreenViewmodel = hiltViewModel()){
     Scaffold(
-//        topBar = {
-//            CustomtopBar()
-//        }
+        topBar = {
+            CustomtopBar(title = "Quizzy")
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {  }, shape = RoundedCornerShape(16.dp), modifier = Modifier.padding(bottom = 20.dp, end = 15.dp)) {
+                Icon(imageVector = Icons.Rounded.Favorite, contentDescription = null)
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
     ) {
         MainContent(mainScreenViewmodel,it)
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomtopBar() {
-    TODO("Not yet implemented")
+fun CustomtopBar(
+    isMainScreen : Boolean = true,
+    title : String,
+    onAction : () -> Unit ={}
+) {
+    CenterAlignedTopAppBar(
+        title = {
+                Text(
+                    text = title,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colors.secondary,
+            navigationIconContentColor = Color.White,
+            titleContentColor = Color.White,
+            actionIconContentColor = Color.White
+        )
+    )
+
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainContent(mainScreenViewmodel: MainScreenViewmodel,it: PaddingValues) {
     val list =mainScreenViewmodel.data.value
+    val pagerState= rememberPagerState()
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         modifier = Modifier
             .fillMaxSize()
             .padding(it)
     ) {
-        if (list.isNotEmpty())
-            QuoteCard(list[0])
+        Spacer(modifier = Modifier.height(120.dp))
+        Text(
+            text = "Swipe right to navigate to the second quote",
+            style = MaterialTheme.typography.body1,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier
+        )
+        Spacer(modifier = Modifier.height(120.dp))
 
+        if (list.isNotEmpty()){
+            QuoteSwipeable(list)
+        }
+
+
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun QuoteSwipeable(list: Quote) {
+    val pagerState= rememberPagerState()
+    HorizontalPager(
+        pageCount = list.size,
+        state = pagerState,
+        key = {list[it].c}
+    ) { index ->
+        QuoteCard(quoteItem = list[index])
     }
 }
 
@@ -86,11 +147,16 @@ fun QuoteCard(quoteItem: QuoteItem) {
                     .align(Alignment.End)
                     .padding(end = 15.dp, bottom = 15.dp)
             )
-            Text(
-                text = "❞",
-                style = MaterialTheme.typography.h2,
-                modifier = Modifier.align(Alignment.End)
-            )
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                IconButton(onClick = {  }) {
+                    Icon(imageVector = Icons.Rounded.AddCircle, contentDescription = "add")
+                }
+                Text(
+                    text = "❞",
+                    style = MaterialTheme.typography.h2,
+                )
+            }
+
         }
     }
 }
